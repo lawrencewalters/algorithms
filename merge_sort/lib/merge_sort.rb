063d72a6e59051383a9a26ef436ed5b7a55d860f
Lawrence Walters <lwalters@enova.com>
2015-10-25 21:55:17 -0500

    split inversions homework

--- merge_sort/lib/merge_sort.rb
+++ merge_sort/lib/merge_sort.rb
@@ -1,38 +1,50 @@
+require "benchmark"
 require 'pp'
 class MergeSort
 	def self.sort(items)
 		if items.length == 1 
-			return items
+			return items, 0
 		end
 		
 	  # cut in half if greater than x size?
 		left, right = items.each_slice((items.size / 2.0).round).to_a
+    split_inversions = 0
 
 		if items.length > 2
-	    left = sort(left)
-			right = sort(right)
+	    left, left_inversions = sort(left)
+			right, right_inversions = sort(right)
+      split_inversions = left_inversions + right_inversions
 		end
 
 		new_array = []
 		lefty = 0
 		righty = 0
-#		puts "items" + items.length.to_s
 		(items.length).times do |k|
-#			puts "#{k}  #{left[lefty]} #{right[righty]}"
 			if righty >= right.length || (left[lefty] && left[lefty] < right[righty])
 				new_array += [left[lefty]]
 				lefty = lefty + 1
 			else
 				new_array += [right[righty]]
-				righty = righty + 1
+        split_inversions = split_inversions + (left.length - lefty)
+        righty = righty + 1
 			end
-		end
-#		pp new_array
-		new_array
+    end
+    #pp items, split_inversions, new_array
+		return new_array, split_inversions
 	end
 end
 
-pp MergeSort.sort([5,4,1,8,7,2,6,3])
-pp MergeSort.sort([5,4,1,8,7,2,6, 6])
-pp MergeSort.sort([5,4,1,8,7,2,6])
-pp MergeSort.sort([5,4,1,8,7,2,66,66])
+# read the array
+#items = IO.read("IntegerArray2.txt").split("\n")
+items = []
+f = File.open("IntegerArray.txt")
+while !f.eof? do
+  items.push(f.readline.strip.to_i)
+end
+#pp items
+puts(Benchmark.measure { items, split_inversions = MergeSort.sort(items); puts split_inversions })
+
+# pp MergeSort.sort([5,4,1,8,7,2,6,3])
+# pp MergeSort.sort([5,4,1,8,7,2,6, 6])
+# pp MergeSort.sort([5,4,1,8,7,2,6])
+# pp MergeSort.sort([5,4,1,8,7,2,66,66])
